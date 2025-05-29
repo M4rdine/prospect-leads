a18b9220a87e03587b0c7801c5c1d781e9ac1216
Raphael Mardine <raphaelmardine@MacBook-Air-de-Raphael.local>
2025-05-29 13:50:12 -0400

    updating to v2, connecting, creating instance, generating qr code and sending message

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body } from '@nestjs/common';
+// src/whatsapp/whatsapp.controller.ts
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
 
 @Controller('whatsapp')
@@ -8,5 +9,20 @@ export class WhatsappController {
   @Post('send')
   async sendMessage(@Body() body: { phone: string; message: string }) {
     return this.whatsappService.sendMessage(body.phone, body.message);
+  }
+
+  @Get('status')
+  async getStatus() {
+    return this.whatsappService.getStatus();
+  }
+
+  @Get('connect')
+  async getConnectInstance() {
+    return this.whatsappService.getConnectInstance();
+  }
+
+  @Post('instance')
+  async createInstance() {
+    return this.whatsappService.createInstance();
   }
 }
--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -1,3 +1,4 @@
+// src/whatsapp/whatsapp.service.ts
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
@@ -12,20 +13,39 @@ export class WhatsappService {
 
   async sendMessage(phone: string, message: string) {
     const url = `${this.BASE_URL}/message/sendText/${this.INSTANCE}`;
-    console.log(url);
     const payload = {
       number: phone,
-      textMessage: { text: message },
+      text: message,
     };
-
-    const headers = {
-      Authorization: `Bearer ${this.TOKEN}`,
-    };
-
-    const response = await firstValueFrom(
-      this.http.post(url, payload, { headers }),
-    );
-
+    const headers = { apikey: process.env.AUTHENTICATION_API_KEY };
+    const response = await firstValueFrom(this.http.post(url, payload, { headers }));
     return response.data;
   }
+
+  async getStatus() {
+    const url = `${this.BASE_URL}/status/${this.INSTANCE}`;
+    const headers = { Authorization: `Bearer ${this.TOKEN}` };
+    const response = await firstValueFrom(this.http.get(url, { headers }));
+    return response.data;
+  }
+
+  async getConnectInstance() {
+    const url = `${this.BASE_URL}/instance/connect/${this.INSTANCE}`;
+    const headers = { apikey: process.env.AUTHENTICATION_API_KEY };
+    const response = await firstValueFrom(this.http.get(url, { headers }));
+    return response.data;
+  }
+
+  async createInstance() {
+    const url = `${this.BASE_URL}/instance/create`;
+    const headers = { apikey: process.env.AUTHENTICATION_API_KEY };
+    const payload = {
+      instanceName: this.INSTANCE,
+      qrcode: true,
+      integration: "WHATSAPP-BAILEYS"
+    }
+    const response = await firstValueFrom(this.http.post(url, payload, { headers }));
+    return response.data;
+  }
+  
 }
